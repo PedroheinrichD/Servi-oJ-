a1f80d7108619ff71dfc836faae1d2bf704c8516
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-08-13 11:03:10 -0300

    feat: cria rota para a tabela serviços consultando os que estão ativos

---
+++ pages/api/servico.js
@@ -0,0 +1,28 @@
+import pool from "../../lib/db";
+
+export default async function handler(req, res) {
+  try {
+    const [rows] = await pool.query('SELECT * FROM servicos WHERE ativo = 1');
+    res.status(200).json(rows)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({erro: 'Erro ao buscar serviços'})
+  }
+}
+
+
+/* 
+    const [rows] = é uma desustruturação do JS, ele
+    basicamente pega o primeiro item da lista retornada
+    da consulta query do banco de dados
+
+    pool é a conexão ao banco de dados
+    query(SELECT * FROM servicos WHERE ativo = 1) é a consulta do banco de dados, 
+    na tabela servicos todos os ativos que tenham o numero 1,
+    significa inativo
+
+    Você nunca escreve handler(req, res) no seu código. 
+    O Next.js faz isso por você, automaticamente. 
+    É o framework trabalhando...
+
+*/
